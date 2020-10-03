26c870121fe2e4330c91fb35ed442eaefb6ecb30
Oliver Pieper <pieper@iaregio.de>
2020-10-03 16:53:47 +0200

    Ctrl+C detection added
    - message "Interrupted at ..." instead of "Finished ar ..."
    - no sound is played

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use chrono::Local;
 use humantime::format_duration;
-use indicatif::{ProgressBar, ProgressStyle};
+use indicatif::{HumanDuration, ProgressBar, ProgressStyle};
 use log::{info, LevelFilter};
 use simple_logging;
 use std::env;
 use std::fs::OpenOptions;
 use std::io::{self, Write};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
 use std::time::{Duration, Instant};
 
 const LOGFILE_NAME: &str = "pomodoros.log";
@@ -15,33 +17,37 @@ struct App {
 }
 
 impl App {
-    fn run(&mut self) {
+    fn run(&mut self, interrupted: &AtomicBool) {
         self.read_args();
-        self.run_pomodoro();
+        self.run_timer(interrupted);
     }
 
-    fn play_sound(&self, duration: Duration) {
+    fn play_sound(duration: Duration) {
         use rodio::Sink;
 
         let device = rodio::default_output_device().unwrap();
         let sink = Sink::new(&device);
 
-        // Add a dummy source of the sake of the example.
-        let source = rodio::source::SineWave::new(440);
-        std::thread::sleep(Duration::from_secs(1));
-        sink.append(source);
+        sink.append(rodio::source::SineWave::new(440));
         std::thread::sleep(duration);
     }
 
-    fn run_pomodoro(&self) {
-        let mini_pomodoro: Duration;
+    fn run_timer(&self, interrupted: &AtomicBool) {
+        fn _info_and_print(msg: &String) {
+            info!("{}", msg);
+            println!("{}", msg);
+        }
+
+        let timer_duration: Duration;
+        let mut was_interrupted: bool = false;
 
         if self.test_mode {
-            mini_pomodoro = Duration::from_secs(6);
+            timer_duration = Duration::from_secs(6);
         } else {
-            mini_pomodoro = chrono::Duration::minutes(25).to_std().unwrap();
+            timer_duration = chrono::Duration::minutes(25).to_std().unwrap();
         }
-        let bar = ProgressBar::new(mini_pomodoro.as_secs());
+
+        let bar = ProgressBar::new(timer_duration.as_secs());
         bar.set_style(
             ProgressStyle::default_bar()
                 .template("{msg} {spinner} [{eta_precise}] [{wide_bar:.red/red}]")
@@ -50,26 +56,41 @@ impl App {
         );
         bar.set_message("🍅");
 
-        let tick = Duration::from_secs(1);
+        let one_second = Duration::from_secs(1);
         let start = Instant::now();
         info!(
             "🍅 Starting {} Pomodoro on {}",
-            format_duration(mini_pomodoro),
+            format_duration(timer_duration),
             Local::now().format("%A, %v at %H:%M:%S")
         );
-        while start.elapsed() < mini_pomodoro {
-            std::thread::sleep(tick);
+        while (start.elapsed() < timer_duration) && !was_interrupted {
+            std::thread::sleep(one_second);
             bar.inc(1);
+            if interrupted.load(Ordering::SeqCst) {
+                was_interrupted = true;
+            }
         }
-        info!("Finished at {}", Local::now().format("%H:%M:%S"));
+
         bar.finish_and_clear();
-        print!("Finished at {}", Local::now().format("%H:%M:%S"));
+
+        if was_interrupted {
+            _info_and_print(&format!(
+                "Interrupted at {} with {} remaining.",
+                Local::now().format("%H:%M:%S"),
+                HumanDuration(timer_duration - start.elapsed())
+            ));
+        } else {
+            _info_and_print(&format!("Finished at {}", Local::now().format("%H:%M:%S")));
+        }
+
         io::stdout().flush().unwrap();
 
-        // zwei Töne, damit meine Soundkarte aufwachen kann ...
-        self.play_sound(Duration::from_secs(2));
-        std::thread::sleep(tick);
-        self.play_sound(Duration::from_secs(1));
+        if !was_interrupted {
+            // two beeps with a pause, to let my speaker wake up ...
+            App::play_sound(one_second);
+            std::thread::sleep(one_second);
+            App::play_sound(one_second);
+        }
     }
 
     fn read_args(&mut self) {
@@ -91,7 +112,15 @@ fn main() {
         LevelFilter::Info,
     );
 
+    let irq = Arc::new(AtomicBool::new(false));
+
+    let irq_c = irq.clone();
+    ctrlc::set_handler(move || {
+        irq_c.store(true, Ordering::SeqCst);
+    })
+    .expect("Error setting Ctrl-C handler");
+
     let mut app = App { test_mode: false };
 
-    app.run();
+    app.run(&irq);
 }
